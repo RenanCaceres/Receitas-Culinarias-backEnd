32756f81aab161ae1ca0e1c0c040e76714f58dc4
Renan Cáceres <149203417+RenanCaceres@users.noreply.github.com>
2026-05-09 20:30:03 -0300

    add arquivos e pastas

---
+++ config/db_sequelize.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('portifolio_receitas', 'postregres', '1234',
+    { host: 'localhost', dialect: 'postgres'});
+
+var db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.Usuario = require('../models/relational/usuario.js') ( sequelize, Sequelize);
+
+module.exports = db;
