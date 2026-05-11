c8c31bdb9f5370178d17c65319ba640306fd9a20
Renan Cáceres <149203417+RenanCaceres@users.noreply.github.com>
2026-05-11 15:46:57 -0300

    upp

--- config/db_mongoose.js
+++ config/db_mongoose.js
@@ -1,5 +1,5 @@
 const StringCon = {
     connection: "mongodb+srv://renan2004:1008@back@cluster0.6xmi7lb.mongodb.net/"
-}
+};
 
 module.exports = StringCon;
--- config/db_sequelize.js
+++ config/db_sequelize.js
@@ -1,21 +1,34 @@
 const Sequelize = require('sequelize');
- 
+
 const sequelize = new Sequelize('portfolio_receitas', 'postgres', '1234', {
     host: 'localhost',
     dialect: 'postgres'
 });
- 
+
 var db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
- 
-db.Usuario    = require('../models/relational/usuario.js')(sequelize, Sequelize);
-db.Receita    = require('../models/relational/receita.js')(sequelize, Sequelize);
-db.Categoria  = require('../models/relational/categoria.js')(sequelize, Sequelize);
-db.Habilidade = require('../models/relational/habilidade.js')(sequelize, Sequelize);
- 
-// Associações
-db.Categoria.hasMany(db.Receita, { foreignKey: 'categoriaId', onDelete: 'NO ACTION' });
+
+// ── Modelos ───────────────────────────────────────────────────────────────────
+db.Usuario        = require('../models/relational/usuario.js')(sequelize, Sequelize);
+db.Receita        = require('../models/relational/receita.js')(sequelize, Sequelize);
+db.Categoria      = require('../models/relational/categoria.js')(sequelize, Sequelize);
+db.Habilidade     = require('../models/relational/habilidade.js')(sequelize, Sequelize);
+db.AlunoHabilidade = require('../models/relational/alunoHabilidade.js')(sequelize, Sequelize);
+db.ReceitaAluno   = require('../models/relational/receitaAluno.js')(sequelize, Sequelize);
+
+// ── Associações ───────────────────────────────────────────────────────────────
+
+// Categoria 1:N Receita
+db.Categoria.hasMany(db.Receita,   { foreignKey: 'categoriaId', onDelete: 'NO ACTION' });
 db.Receita.belongsTo(db.Categoria, { foreignKey: 'categoriaId' });
- 
+
+// Usuario N:N Habilidade  (tabela: alunoHabilidade)
+db.Usuario.belongsToMany(db.Habilidade, { through: db.AlunoHabilidade, foreignKey: 'usuarioId' });
+db.Habilidade.belongsToMany(db.Usuario, { through: db.AlunoHabilidade, foreignKey: 'habilidadeId' });
+
+// Usuario N:N Receita  (tabela: receitaAluno)
+db.Receita.belongsToMany(db.Usuario, { through: db.ReceitaAluno, foreignKey: 'receitaId' });
+db.Usuario.belongsToMany(db.Receita, { through: db.ReceitaAluno, foreignKey: 'usuarioId' });
+
 module.exports = db;
