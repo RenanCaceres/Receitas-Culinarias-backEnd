d482e6b13e9a24e4ad09e92191264b59d6660c69
Renan Cáceres <149203417+RenanCaceres@users.noreply.github.com>
2026-05-14 15:12:26 -0300

    Update final

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(middlewares.sessionControl);
 
 app.use('/', route);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
