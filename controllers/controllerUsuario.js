c5a702b06c093b859f23120aaf407b4851b59a97
Renan Cáceres <149203417+RenanCaceres@users.noreply.github.com>
2026-05-14 22:12:31 -0300

    up

--- controllers/controllerUsuario.js
+++ controllers/controllerUsuario.js
@@ -1,49 +1,195 @@
-async postSalvarHabilidades(req, res) {
+const db = require('../config/db_sequelize');
 
-    try {
+module.exports = {
+    async getLogin(req, res) {
+        res.render('usuario/login', { layout: 'noMenu' });
+    },
 
-        const alunoId = req.session.alunoId;
-
-        let habilidades = req.body.habilidades || [];
-
-        // SE VIER APENAS UMA HABILIDADE
-        if (!Array.isArray(habilidades)) {
-            habilidades = [habilidades];
-        }
-
-        // REMOVE AS ANTIGAS
-        await db.AlunoHabilidade.destroy({
-            where: { usuarioId: alunoId }
-        });
-
-        const registros = [];
-
-        for (const id of habilidades) {
-
-            const nivel = Number(req.body[`nivel_${id}`] || 0);
-
-            registros.push({
-                usuarioId: alunoId,
-                habilidadeId: Number(id),
-                nivel: nivel
+    async postLogin(req, res) {
+        try {
+            const usuario = await db.Usuario.findOne({
+                where: {
+                    login: req.body.login,
+                    senha: req.body.senha
+                }
             });
 
+            if (!usuario) {
+                return res.render('usuario/login', {
+                    layout: 'noMenu',
+                    erro: 'Login ou senha inválidos.'
+                });
+            }
+
+            req.session.login = usuario.login;
+            req.session.alunoId = usuario.id;
+            req.session.tipo = usuario.tipo;
+
+            res.redirect('/home');
+        } catch (err) {
+            console.log(err);
+            res.render('usuario/login', {
+                layout: 'noMenu',
+                erro: 'Erro ao fazer login.'
+            });
         }
+    },
 
-        // SALVA
-        if (registros.length > 0) {
+    async getLogout(req, res) {
+        req.session.destroy(() => {
+            res.redirect('/');
+        });
+    },
 
-            await db.AlunoHabilidade.bulkCreate(registros);
+    async getCreate(req, res) {
+        res.render('usuario/usuarioCreate');
+    },
 
+    async postCreate(req, res) {
+        try {
+            await db.Usuario.create(req.body);
+            res.redirect('/usuarioList');
+        } catch (err) {
+            console.log(err);
+            res.redirect('/usuarioCreate');
         }
+    },
 
-        res.redirect('/minhasHabilidades');
+    async getList(req, res) {
+        try {
+            const usuarios = await db.Usuario.findAll({
+                order: [['id', 'ASC']]
+            });
 
-    } catch (err) {
+            res.render('usuario/usuarioList', {
+                usuarios: usuarios.map(usuario => usuario.toJSON())
+            });
+        } catch (err) {
+            console.log(err);
+            res.redirect('/home');
+        }
+    },
 
-        console.log(err);
+    async getUpdate(req, res) {
+        try {
+            const usuario = await db.Usuario.findByPk(req.params.id);
 
-        res.redirect('/minhasHabilidades');
+            if (!usuario) {
+                return res.redirect('/usuarioList');
+            }
 
+            res.render('usuario/usuarioUpdate', {
+                usuario: usuario.toJSON()
+            });
+        } catch (err) {
+            console.log(err);
+            res.redirect('/usuarioList');
+        }
+    },
+
+    async postUpdate(req, res) {
+        try {
+            await db.Usuario.update(req.body, {
+                where: {
+                    id: req.body.id
+                }
+            });
+
+            res.redirect('/usuarioList');
+        } catch (err) {
+            console.log(err);
+            res.redirect('/usuarioList');
+        }
+    },
+
+    async getDelete(req, res) {
+        try {
+            await db.Usuario.destroy({
+                where: {
+                    id: req.params.id
+                }
+            });
+
+            res.redirect('/usuarioList');
+        } catch (err) {
+            console.log(err);
+            res.redirect('/usuarioList');
+        }
+    },
+
+    async getMinhasHabilidades(req, res) {
+        try {
+            const habilidades = await db.Habilidade.findAll({
+                order: [['nome', 'ASC']]
+            });
+
+            const minhas = await db.AlunoHabilidade.findAll({
+                where: {
+                    usuarioId: req.session.alunoId
+                }
+            });
+
+            const mapa = {};
+
+            minhas.forEach(item => {
+                mapa[item.habilidadeId] = item.nivel;
+            });
+
+            const todasHabilidades = habilidades.map(habilidade => ({
+                ...habilidade.toJSON(),
+                selecionada: mapa[habilidade.id] !== undefined,
+                nivel: mapa[habilidade.id] !== undefined ? mapa[habilidade.id] : 0
+            }));
+
+            res.render('usuario/minhasHabilidades', {
+                todasHabilidades
+            });
+        } catch (err) {
+            console.log(err);
+            res.redirect('/home');
+        }
+    },
+
+    async postSalvarHabilidades(req, res) {
+        try {
+            console.log('BODY:', req.body);
+
+            const alunoId = req.session.alunoId;
+
+            let habilidades = req.body.habilidades || [];
+
+            if (!Array.isArray(habilidades)) {
+                habilidades = [habilidades];
+            }
+
+            await db.AlunoHabilidade.destroy({
+                where: {
+                    usuarioId: alunoId
+                }
+            });
+
+            const registros = [];
+
+            for (const id of habilidades) {
+                const nivel = Number(req.body[`nivel_${id}`] || 0);
+
+                registros.push({
+                    usuarioId: alunoId,
+                    habilidadeId: Number(id),
+                    nivel: Math.max(0, Math.min(10, nivel))
+                });
+            }
+
+            console.log('REGISTROS:', registros);
+
+            if (registros.length > 0) {
+                await db.AlunoHabilidade.bulkCreate(registros);
+            }
+
+            res.redirect('/minhasHabilidades');
+        } catch (err) {
+            console.log(err);
+            res.redirect('/minhasHabilidades');
+        }
     }
-}
+};
